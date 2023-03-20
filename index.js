321260d12633606ff062c9018ab9df2ab178e0ef
Halim Choi <hchoicareer@gmail.com>
2023-03-19 23:43:36 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num) {
+  let message;
+  if (num <= 400) {
+    message = "This one is on me!";
+  }
+  else if (num >= 400 && num <= 2000) {
+    message = "That will be twenty bucks.";
+  }
+  else if (num > 2000 && num < 2500) {
+    message = "I will gladly take your thirty bucks.";
+  }
+  else if (num > 2500) {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let message;
+  if (city == "NYC") {
+    message = "Ok, sounds good.";
+  }
+  else {
+    message = "No go.";
+  }
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let message;
+  if (tip == "generous") {
+    message = "Thank you so much.";
+  }
+  else if (tip == "not as generous") {
+    message = "Thank you."
+  }
+  else {
+    message = "Bye.";
+  }
+  return message;
 }
